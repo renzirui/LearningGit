b9177db084b34c03f6e95c4080a913adf1fe9b11
gyq <guoyq1104@thundersoft.com>
2019-12-02 15:27:46 +0800

    guess what I have modified

--- HashMap.java
+++ HashMap.java
@@ -137,7 +137,7 @@ import java.util.function.Function;
 public class HashMap<K,V> extends AbstractMap<K,V>
     implements Map<K,V>, Cloneable, Serializable {
 
-    private static final long serialVersionUID = 362498820763181265L;
+    private static int serialVersionUID = 362498820763181265L;
 
     /*
      * Implementation notes.
